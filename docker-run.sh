b64597e908edec03e622cf8bb7eaed5aacf177a5
Markus Witt <feuerrot@feuerrot.org>
2019-04-16 18:48:32 +0200

    config: mail server setup

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,16 @@ then
 	exit 1
 fi
 
+if [ \
+	"${TRAVELYNX_MAIL_DISABLE:-0}" -eq 0 \
+	-a "${TRAVELYNX_MAIL_HOST:-unset}" != "unset" \
+]
+then
+	export EMAIL_SENDER_TRANSPORT=SMTP
+	export EMAIL_SENDER_TRANSPORT_HOST=${TRAVELYNX_MAIL_HOST}
+	export EMAIL_SENDER_TRANSPORT_PORT=${TRAVELYNX_MAIL_PORT:-25}
+fi
+
 perl index.pl database migrate
 
 exec /usr/local/bin/hypnotoad -f index.pl
