46e85324df762672718c0a6a5ca60fb247e89e5d
Markus Witt <feuerrot@feuerrot.org>
2019-04-21 23:13:03 +0200

    docker: add docker-compose configuration
    🔥docker🔥compose🔥

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
+
+WAIT_DB_HOST=${TRAVELYNX_DB_HOST}
+WAIT_DB_PORT=5432
+
+wait_for_db() {
+	set +e
+	for i in $(seq 1 ${WAIT_DB_TIMEOUT:-5})
+	do
+		(echo >/dev/tcp/${WAIT_DB_HOST}/${WAIT_DB_PORT}) &>/dev/null
+		if [ $? -eq 0 ]; then
+		    break
+		fi
+		sleep 1
+	done
+	set -e
+}
 
 if [ ! -f travelynx.conf ]
 then
@@ -16,6 +32,8 @@ then
 	export EMAIL_SENDER_TRANSPORT_HOST=${TRAVELYNX_MAIL_HOST}
 	export EMAIL_SENDER_TRANSPORT_PORT=${TRAVELYNX_MAIL_PORT:-25}
 fi
+
+wait_for_db
 
 perl index.pl database migrate
 
