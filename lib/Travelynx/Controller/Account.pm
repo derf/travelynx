954638e7c75342e081a25da398239b63a0e71d41
Daniel Friesel <derf@finalrewind.org>
2019-05-03 21:14:49 +0200

    data export: use station names, not IDs

--- lib/Travelynx/Controller/Account.pm
+++ lib/Travelynx/Controller/Account.pm
@@ -572,11 +572,11 @@ sub json_export {
 		json => {
 			account    => $db->select( 'users', '*', { id => $uid } )->hash,
 			in_transit => [
-				$db->select( 'in_transit', '*', { user_id => $uid } )
+				$db->select( 'in_transit_str', '*', { user_id => $uid } )
 				  ->hashes->each
 			],
 			journeys => [
-				$db->select( 'journeys', '*', { user_id => $uid } )
+				$db->select( 'journeys_str', '*', { user_id => $uid } )
 				  ->hashes->each
 			],
 		}
