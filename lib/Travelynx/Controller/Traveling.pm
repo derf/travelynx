derf/travelynx

commit 7c7b5e9f9536f70c92d66c4055090b46e26c4c11

Do not show station duplicates in geolocation list

lib/Travelynx/Controller/Traveling.pm
@@ -3,6 +3,7 @@ use Mojo::Base 'Mojolicious::Controller';
 
 use DateTime;
 use DateTime::Format::Strptime;
+use List::UtilsBy qw(uniq_by);
 use Travel::Status::DE::IRIS::Stations;
 
 sub homepage {
@@ -137,10 +138,11 @@ sub geolocation {
 				distance => $_->[1],
 			}
 		} Travel::Status::DE::IRIS::Stations::get_station_by_location( $lon,
-			$lat, 5 );
+			$lat, 10 );
+		@candidates = uniq_by { $_->{name} } @candidates;
 		$self->render(
 			json => {
-				candidates => [@candidates],
+				candidates => [ @candidates[ 0 .. 4 ] ],
 			}
 		);
 	}

cpanfile
@@ -5,6 +5,7 @@ requires 'DateTime::Format::Strptime';
 requires 'Email::Sender';
 requires 'Geo::Distance';
 requires 'Geo::Distance::XS';
+requires 'List::UtilsBy';
 requires 'Mojolicious';
 requires 'Mojolicious::Plugin::Authentication';
 requires 'Mojo::Pg';

commit 4ba82b51d301fff67728b8860d409d92cc11b352

Fix invalid geolocation list when less than 5 unique station names are nearby
This issue was introduced in 7c7b5e9f9536f70c92d66c4055090b46e26c4c11 and
caused the client-side javascript code to crash

lib/Travelynx/Controller/Traveling.pm
@@ -140,11 +140,20 @@ sub geolocation {
 		} Travel::Status::DE::IRIS::Stations::get_station_by_location( $lon,
 			$lat, 10 );
 		@candidates = uniq_by { $_->{name} } @candidates;
-		$self->render(
-			json => {
-				candidates => [ @candidates[ 0 .. 4 ] ],
-			}
-		);
+		if ( @candidates > 5 ) {
+			$self->render(
+				json => {
+					candidates => [ @candidates[ 0 .. 4 ] ],
+				}
+			);
+		}
+		else {
+			$self->render(
+				json => {
+					candidates => [@candidates],
+				}
+			);
+		}
 	}
 }
 