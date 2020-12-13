5b7086bcd96e06113410e9559e92d62a061e12de
Daniel Friesel <derf@finalrewind.org>
2020-12-13 21:51:59 +0100

    update station list for autocompletion

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = 'static-cache-v35';
+const CACHE_NAME = 'static-cache-v36';
 const FILES_TO_CACHE = [
   '/favicon.ico',
   '/offline.html',
-  '/static/v35/css/light.min.css',
-  '/static/v35/css/dark.min.css',
-  '/static/v35/css/material-icons.css',
-  '/static/v35/css/local.css',
-  '/static/v35/fonts/MaterialIcons-Regular.woff2',
-  '/static/v35/fonts/MaterialIcons-Regular.woff',
-  '/static/v35/fonts/MaterialIcons-Regular.ttf',
-  '/static/v35/js/jquery-3.4.1.min.js',
-  '/static/v35/js/materialize.min.js',
-  '/static/v35/js/travelynx-actions.min.js',
-  '/static/v35/js/autocomplete.min.js',
-  '/static/v35/js/geolocation.min.js',
+  '/static/v36/css/light.min.css',
+  '/static/v36/css/dark.min.css',
+  '/static/v36/css/material-icons.css',
+  '/static/v36/css/local.css',
+  '/static/v36/fonts/MaterialIcons-Regular.woff2',
+  '/static/v36/fonts/MaterialIcons-Regular.woff',
+  '/static/v36/fonts/MaterialIcons-Regular.ttf',
+  '/static/v36/js/jquery-3.4.1.min.js',
+  '/static/v36/js/materialize.min.js',
+  '/static/v36/js/travelynx-actions.min.js',
+  '/static/v36/js/autocomplete.min.js',
+  '/static/v36/js/geolocation.min.js',
 ];
 
 self.addEventListener('install', (evt) => {
