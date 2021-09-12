2f01ea6f0daa5cb7d2213d2effee46f9399293ad
Daniel Friesel <derf@finalrewind.org>
2021-09-12 10:09:46 +0200

    Update station name autocompletion for T-S-DE-IRIS 1.60

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = 'static-cache-v37';
+const CACHE_NAME = 'static-cache-v38';
 const FILES_TO_CACHE = [
   '/favicon.ico',
   '/offline.html',
-  '/static/v37/css/light.min.css',
-  '/static/v37/css/dark.min.css',
-  '/static/v37/css/material-icons.css',
-  '/static/v37/css/local.css',
-  '/static/v37/fonts/MaterialIcons-Regular.woff2',
-  '/static/v37/fonts/MaterialIcons-Regular.woff',
-  '/static/v37/fonts/MaterialIcons-Regular.ttf',
-  '/static/v37/js/jquery-3.4.1.min.js',
-  '/static/v37/js/materialize.min.js',
-  '/static/v37/js/travelynx-actions.min.js',
-  '/static/v37/js/autocomplete.min.js',
-  '/static/v37/js/geolocation.min.js',
+  '/static/v38/css/light.min.css',
+  '/static/v38/css/dark.min.css',
+  '/static/v38/css/material-icons.css',
+  '/static/v38/css/local.css',
+  '/static/v38/fonts/MaterialIcons-Regular.woff2',
+  '/static/v38/fonts/MaterialIcons-Regular.woff',
+  '/static/v38/fonts/MaterialIcons-Regular.ttf',
+  '/static/v38/js/jquery-3.4.1.min.js',
+  '/static/v38/js/materialize.min.js',
+  '/static/v38/js/travelynx-actions.min.js',
+  '/static/v38/js/autocomplete.min.js',
+  '/static/v38/js/geolocation.min.js',
 ];
 
 self.addEventListener('install', (evt) => {
