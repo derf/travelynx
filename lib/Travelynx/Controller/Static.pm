5d8f6467dc00f03957784230983ed730ec421291
Cassidy Dingenskirchen <admin@15318.de>
2024-01-22 18:53:27 +0100

    add title elements

--- lib/Travelynx/Controller/Static.pm
+++ lib/Travelynx/Controller/Static.pm
@@ -8,25 +8,25 @@ use Mojo::Base 'Mojolicious::Controller';
 sub about {
 	my ($self) = @_;
 
-	$self->render('about');
+	$self->render('about', title => 'Über travelynx');
 }
 
 sub changelog {
 	my ($self) = @_;
 
-	$self->render('changelog');
+	$self->render('changelog',  title => 'travelynx: Changelog');
 }
 
 sub imprint {
 	my ($self) = @_;
 
-	$self->render('imprint');
+	$self->render('imprint',  title => 'travelynx: Impressum');
 }
 
 sub legend {
 	my ($self) = @_;
 
-	$self->render('legend');
+	$self->render('legend', title => 'travelynx: Legende' );
 }
 
 sub offline {
