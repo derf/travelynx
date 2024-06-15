b00c3698b2a9a8702a02c6c978560aad6cff8698
Birte Kristina Friesel <derf@finalrewind.org>
2024-06-15 10:43:47 +0200

    traewelling: increase latency between traewelling.de requests

--- lib/Travelynx/Command/traewelling.pm
+++ lib/Travelynx/Command/traewelling.pm
@@ -52,7 +52,7 @@ sub pull_sync {
 
 		# In 'work', the event loop is not running,
 		# so there's no need to multiply by $request_count at the moment
-		Mojo::Promise->timer(1)->then(
+		Mojo::Promise->timer(1.5)->then(
 			sub {
 				return $self->app->traewelling_api->get_status_p(
 					username => $account_data->{data}{user_name},
