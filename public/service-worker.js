c5bea4e866b246144396011cb267ed5e258c3263
Birte Kristina Friesel <derf@finalrewind.org>
2024-06-30 14:37:15 +0200

    release assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'static-cache-v74';
+const CACHE_NAME = 'static-cache-v75';
 const FILES_TO_CACHE = [
   '/favicon.ico',
   '/offline.html',
-  '/static/v74/css/light.min.css',
-  '/static/v74/css/dark.min.css',
-  '/static/v74/css/material-icons.css',
-  '/static/v74/fonts/MaterialIcons-Regular.woff2',
-  '/static/v74/fonts/MaterialIcons-Regular.woff',
-  '/static/v74/fonts/MaterialIcons-Regular.ttf',
-  '/static/v74/js/jquery-3.4.1.min.js',
-  '/static/v74/js/materialize.min.js',
-  '/static/v74/js/travelynx-actions.min.js',
-  '/static/v74/js/autocomplete.min.js',
-  '/static/v74/js/geolocation.min.js',
+  '/static/v75/css/light.min.css',
+  '/static/v75/css/dark.min.css',
+  '/static/v75/css/material-icons.css',
+  '/static/v75/fonts/MaterialIcons-Regular.woff2',
+  '/static/v75/fonts/MaterialIcons-Regular.woff',
+  '/static/v75/fonts/MaterialIcons-Regular.ttf',
+  '/static/v75/js/jquery-3.4.1.min.js',
+  '/static/v75/js/materialize.min.js',
+  '/static/v75/js/travelynx-actions.min.js',
+  '/static/v75/js/autocomplete.min.js',
+  '/static/v75/js/geolocation.min.js',
 ];
 
 self.addEventListener('install', (evt) => {
