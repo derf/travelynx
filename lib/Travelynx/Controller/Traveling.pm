derf/travelynx

commit c250a2f2c7968966014315f76b25109b83c041ed

Add experimental EFA support
Squashed commit of the following:

commit b7457791ab7ab4859ebf4a5ce173e1aaeed4c7fb
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sun Jun 15 08:18:46 2025 +0200

    changelog

commit 7f3d61066195cfc3c83a8fc1b2fc3743e7e6171c
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 19:55:51 2025 +0200

    Mark EFA backends as experimental for now

    For instance, VRR has very interesting issues when checking into departures
    that do not have real-time data yet.

commit 3370c0f6c25bd6b02cc4d56e9a3aba2a66d1151a
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 19:49:48 2025 +0200

    InTransit: remove debug output

commit deb5444fa2965228b537e86fce862436ef2e6e19
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 19:12:44 2025 +0200

    frontend js for checked-in view: never show fractional delays

commit d47ff9615b551bbd844a799be7717e9e74a04266
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 19:12:31 2025 +0200

    worker: add EFA support

commit 3a955c0105bf13d040a821e2c87a19694202cde6
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 17:48:46 2025 +0200

    EFA: checkin support

    worker support and cancellations are still missing

commit 19dea1ad13029d19cba38e7d1338718149c139fb
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 14:32:59 2025 +0200

    actions.js: pass on efa parameter

commit 8f18ff2c8f9f906a387dbe16d372e1c4b4a6f259
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 14:32:48 2025 +0200

    EFA: implement geolocation lookup

commit bce1139bab9aab167cdab910fa86085529d45b80
Author: Birte Kristina Friesel <derf@finalrewind.org>
Date:   Sat Jun 14 14:32:21 2025 +0200

    EFA: ->id is no longer supported, use ->id_num

commit e4397e6b1538ddfa71da9839d6011a73fadc528f
Author: Birte Kristina Friesel <derf@finalrewind.org>

lib/Travelynx/Controller/Traveling.pm
@@ -562,10 +562,13 @@ sub geolocation {
 		return;
 	}
 
-	my ( $dbris_service, $hafas_service, $motis_service );
+	my ( $dbris_service, $efa_service, $hafas_service, $motis_service );
 	my $backend = $self->stations->get_backend( backend_id => $backend_id );
 	if ( $backend->{dbris} ) {
 		$dbris_service = $backend->{name};
+	}
+	if ( $backend->{efa} ) {
+		$efa_service = $backend->{name};
 	}
 	elsif ( $backend->{hafas} ) {
 		$hafas_service = $backend->{name};
@@ -595,6 +598,50 @@ sub geolocation {
 						dbris    => $dbris_service,
 					}
 				} $dbris->results;
+				if ( @results > 10 ) {
+					@results = @results[ 0 .. 9 ];
+				}
+				$self->render(
+					json => {
+						candidates => [@results],
+					}
+				);
+			}
+		)->catch(
+			sub {
+				my ($err) = @_;
+				$self->render(
+					json => {
+						candidates => [],
+						warning    => $err,
+					}
+				);
+			}
+		)->wait;
+		return;
+	}
+	elsif ($efa_service) {
+		$self->render_later;
+
+		Travel::Status::DE::EFA->new_p(
+			promise    => 'Mojo::Promise',
+			user_agent => Mojo::UserAgent->new,
+			service    => $efa_service,
+			coord      => {
+				lat => $lat,
+				lon => $lon
+			}
+		)->then(
+			sub {
+				my ($efa) = @_;
+				my @results = map {
+					{
+						name     => $_->full_name,
+						eva      => $_->id_code,
+						distance => 0,
+						efa      => $efa_service,
+					}
+				} $efa->results;
 				if ( @results > 10 ) {
 					@results = @results[ 0 .. 9 ];
 				}
@@ -726,8 +773,6 @@ sub geolocation {
 			lon      => $_->[0][3],
 			lat      => $_->[0][4],
 			distance => $_->[1],
-			dbris    => 0,
-			hafas    => 0,
 		}
 	} Travel::Status::DE::IRIS::Stations::get_station_by_location( $lon,
 		$lat, 10 );
@@ -796,6 +841,7 @@ sub travel_action {
 			sub {
 				return $self->checkin_p(
 					dbris        => $params->{dbris},
+					efa          => $params->{efa},
 					hafas        => $params->{hafas},
 					motis        => $params->{motis},
 					station      => $params->{station},
@@ -831,6 +877,9 @@ sub travel_action {
 					my $station_link = '/s/' . $destination;
 					if ( $status->{is_dbris} ) {
 						$station_link .= '?dbris=' . $status->{backend_name};
+					}
+					elsif ( $status->{is_efa} ) {
+						$station_link .= '?efa=' . $status->{backend_name};
 					}
 					elsif ( $status->{is_hafas} ) {
 						$station_link .= '?hafas=' . $status->{backend_name};
@@ -870,6 +919,9 @@ sub travel_action {
 				my $station_link = '/s/' . $params->{station};
 				if ( $status->{is_dbris} ) {
 					$station_link .= '?dbris=' . $status->{backend_name};
+				}
+				elsif ( $status->{is_efa} ) {
+					$station_link .= '?efa=' . $status->{backend_name};
 				}
 				elsif ( $status->{is_hafas} ) {
 					$station_link .= '?hafas=' . $status->{backend_name};
@@ -929,6 +981,12 @@ sub travel_action {
 					  . '?dbris='
 					  . $status->{backend_name};
 				}
+				elsif ( $status->{is_efa} ) {
+					$redir
+					  = '/s/'
+					  . $status->{dep_eva} . '?efa='
+					  . $status->{backend_name};
+				}
 				elsif ( $status->{is_hafas} ) {
 					$redir
 					  = '/s/'
@@ -959,6 +1017,7 @@ sub travel_action {
 		$self->render_later;
 		$self->checkin_p(
 			dbris    => $params->{dbris},
+			efa      => $params->{efa},
 			hafas    => $params->{hafas},
 			motis    => $params->{motis},
 			station  => $params->{station},
@@ -1091,6 +1150,8 @@ sub station {
 
 	my $dbris_service = $self->param('dbris')
 	  // ( $user->{backend_dbris} ? $user->{backend_name} : undef );
+	my $efa_service = $self->param('efa')
+	  // ( $user->{backend_efa} ? $user->{backend_name} : undef );
 	my $hafas_service = $self->param('hafas')
 	  // ( $user->{backend_hafas} ? $user->{backend_name} : undef );
 	my $motis_service = $self->param('motis')
@@ -1116,6 +1177,15 @@ sub station {
 			station    => $station,
 			timestamp  => $timestamp,
 			lookbehind => 30,
+		);
+	}
+	elsif ($efa_service) {
+		$promise = $self->efa->get_departures_p(
+			service    => $efa_service,
+			name       => $station,
+			timestamp  => $timestamp,
+			lookbehind => 30,
+			lookahead  => 30,
 		);
 	}
 	elsif ($hafas_service) {
@@ -1209,6 +1279,16 @@ sub station {
 					related_stations => [],
 				};
 			}
+			elsif ($efa_service) {
+				@results = map { $_->[0] }
+				  sort { $b->[1] <=> $a->[1] }
+				  map { [ $_, $_->datetime->epoch ] } $status->results;
+				$status = {
+					station_eva      => $status->stop->id_num,
+					station_name     => $status->stop->full_name,
+					related_stations => [],
+				};
+			}
 			elsif ($motis_service) {
 				@results = map { $_->[0] }
 				  sort { $b->[1] <=> $a->[1] }
@@ -1268,12 +1348,14 @@ sub station {
 						eva => $user_status->{cancellation}{dep_eva},
 						destination_name =>
 						  $user_status->{cancellation}{arr_name},
+						efa   => $efa_service,
 						hafas => $hafas_service,
 					);
 				}
 				else {
 					$connections_p = $self->get_connecting_trains_p(
 						eva   => $status->{station_eva},
+						efa   => $efa_service,
 						hafas => $hafas_service
 					);
 				}
@@ -1287,6 +1369,7 @@ sub station {
 							'departures',
 							user              => $user,
 							dbris             => $dbris_service,
+							efa               => $efa_service,
 							hafas             => $hafas_service,
 							motis             => $motis_service,
 							eva               => $status->{station_eva},
@@ -1308,6 +1391,7 @@ sub station {
 							'departures',
 							user             => $user,
 							dbris            => $dbris_service,
+							efa              => $efa_service,
 							hafas            => $hafas_service,
 							motis            => $motis_service,
 							eva              => $status->{station_eva},
@@ -1328,6 +1412,7 @@ sub station {
 					'departures',
 					user             => $user,
 					dbris            => $dbris_service,
+					efa              => $efa_service,
 					hafas            => $hafas_service,
 					motis            => $motis_service,
 					eva              => $status->{station_eva},

cpanfile
@@ -17,6 +17,7 @@ requires 'Mojolicious::Plugin::OAuth2';
 requires 'Mojo::Pg';
 requires 'Text::CSV';
 requires 'Text::Markdown';
+requires 'Travel::Status::DE::EFA';
 requires 'Travel::Status::MOTIS', '>= 0.01';
 requires 'Travel::Status::DE::DBRIS', '>= 0.10';
 requires 'Travel::Status::DE::HAFAS', '>= 6.20';

lib/Travelynx.pm
@@ -23,6 +23,7 @@ use List::MoreUtils qw(first_index);
 use Travel::Status::DE::DBRIS::Formation;
 use Travelynx::Helper::DBDB;
 use Travelynx::Helper::DBRIS;
+use Travelynx::Helper::EFA;
 use Travelynx::Helper::HAFAS;
 use Travelynx::Helper::IRIS;
 use Travelynx::Helper::MOTIS;
@@ -160,11 +161,12 @@ sub startup {
 		cache_iris_main => sub {
 			my ($self) = @_;
 
-			return Cache::File->new(
+			state $cache = Cache::File->new(
 				cache_root      => $self->app->config->{cache}->{schedule},
 				default_expires => '6 hours',
 				lock_level      => Cache::File::LOCK_LOCAL(),
 			);
+			return $cache;
 		}
 	);
 
@@ -172,11 +174,12 @@ sub startup {
 		cache_iris_rt => sub {
 			my ($self) = @_;
 
-			return Cache::File->new(
+			state $cache = Cache::File->new(
 				cache_root      => $self->app->config->{cache}->{realtime},
 				default_expires => '70 seconds',
 				lock_level      => Cache::File::LOCK_LOCAL(),
 			);
+			return $cache;
 		}
 	);
 
@@ -194,7 +197,7 @@ sub startup {
 
 	$self->attr(
 		renamed_station => sub {
-			my $legacy_to_new = JSON->new->utf8->decode(
+			state $legacy_to_new = JSON->new->utf8->decode(
 				scalar read_file('share/old_station_names.json') );
 			return $legacy_to_new;
 		}
@@ -216,6 +219,20 @@ sub startup {
 			}
 			return $self->url_for($path)
 			  ->base( $self->app->config->{base_url} );
+		}
+	);
+
+	$self->helper(
+		efa => sub {
+			my ($self) = @_;
+			state $efa = Travelynx::Helper::EFA->new(
+				log            => $self->app->log,
+				main_cache     => $self->app->cache_iris_main,
+				realtime_cache => $self->app->cache_iris_rt,
+				root_url       => $self->base_url_for('/')->to_abs,
+				user_agent     => $self->ua,
+				version        => $self->app->config->{version},
+			);
 		}
 	);
 
@@ -490,14 +507,17 @@ sub startup {
 				return Mojo::Promise->reject('You are already checked in');
 			}
 
-			if ( $opt{motis} ) {
-				return $self->_checkin_motis_p(%opt);
-			}
 			if ( $opt{dbris} ) {
 				return $self->_checkin_dbris_p(%opt);
 			}
+			if ( $opt{efa} ) {
+				return $self->_checkin_efa_p(%opt);
+			}
 			if ( $opt{hafas} ) {
 				return $self->_checkin_hafas_p(%opt);
+			}
+			if ( $opt{motis} ) {
+				return $self->_checkin_motis_p(%opt);
 			}
 
 			my $promise = Mojo::Promise->new;
@@ -869,6 +889,137 @@ sub startup {
 	);
 
 	$self->helper(
+		'_checkin_efa_p' => sub {
+			my ( $self, %opt ) = @_;
+			my $station = $opt{station};
+			my $trip_id = $opt{train_id};
+			my $ts      = $opt{ts};
+			my $uid     = $opt{uid} // $self->current_user->{id};
+			my $db      = $opt{db}  // $self->pg->db;
+
+			my $promise = Mojo::Promise->new;
+			$self->efa->get_journey_p(
+				service => $opt{efa},
+				trip_id => $trip_id
+			)->then(
+				sub {
+					my ($journey) = @_;
+
+					my $found;
+					for my $stop ( $journey->route ) {
+						if ( $stop->id_num == $station ) {
+							$found = $stop;
+
+							# Lines may serve the same stop several times.
+							# Keep looking until the scheduled departure
+							# matches the one passed while checking in.
+							if ( $ts and $stop->sched_dep->epoch == $ts ) {
+								last;
+							}
+						}
+					}
+					if ( not $found ) {
+						$promise->reject(
+"Did not find stop '$station' within journey '$trip_id'"
+						);
+						return;
+					}
+
+					for my $stop ( $journey->route ) {
+						$self->stations->add_or_update(
+							stop => $stop,
+							db   => $db,
+							efa  => $opt{efa},
+						);
+					}
+
+					eval {
+						$self->in_transit->add(
+							uid        => $uid,
+							db         => $db,
+							journey    => $journey,
+							stop       => $found,
+							trip_id    => $trip_id,
+							backend_id => $self->stations->get_backend_id(
+								efa => $opt{efa}
+							),
+						);
+					};
+					if ($@) {
+						$self->app->log->error(
+							"Checkin($uid): INSERT failed: $@");
+						$promise->reject( 'INSERT failed: ' . $@ );
+						return;
+					}
+
+					my $polyline;
+					if ( $journey->polyline ) {
+						my @station_list;
+						my @coordinate_list;
+						for my $coord ( $journey->polyline ) {
+							if ( $coord->{stop} ) {
+								push(
+									@coordinate_list,
+									[
+										$coord->{lon}, $coord->{lat},
+										$coord->{stop}->id_num
+									]
+								);
+								push( @station_list,
+									$coord->{stop}->full_name );
+							}
+							else {
+								push( @coordinate_list,
+									[ $coord->{lon}, $coord->{lat} ] );
+							}
+						}
+
+						# equal length → polyline only consists of straight
+						# lines between stops. that's not helpful.
+						if ( @station_list == @coordinate_list ) {
+							$self->log->debug( 'Ignoring polyline for '
+								  . $journey->line
+								  . ' as it only consists of straight lines between stops.'
+							);
+						}
+						else {
+							$polyline = {
+								from_eva => ( $journey->route )[0]->id_num,
+								to_eva   => ( $journey->route )[-1]->id_num,
+								coords   => \@coordinate_list,
+							};
+						}
+					}
+
+					if ($polyline) {
+						$self->in_transit->set_polyline(
+							uid      => $uid,
+							db       => $db,
+							polyline => $polyline,
+						);
+					}
+
+					# mustn't be called during a transaction
+					if ( not $opt{in_transaction} ) {
+						$self->run_hook( $uid, 'checkin' );
+					}
+
+					$promise->resolve($journey);
+
+					return;
+				}
+			)->catch(
+				sub {
+					my ($err) = @_;
+					$promise->reject($err);
+					return;
+				}
+			)->wait;
+			return $promise;
+		}
+	);
+
+	$self->helper(
 		'_checkin_hafas_p' => sub {
 			my ( $self, %opt ) = @_;
 
@@ -877,7 +1028,6 @@ sub startup {
 			my $ts       = $opt{ts};
 			my $uid      = $opt{uid} // $self->current_user->{id};
 			my $db       = $opt{db}  // $self->pg->db;
-			my $hafas;
 
 			my $promise = Mojo::Promise->new;
 
@@ -1136,7 +1286,11 @@ sub startup {
 				return $promise->resolve( 0, 'race condition' );
 			}
 
-			if ( $user->{is_dbris} or $user->{is_hafas} or $user->{is_motis} ) {
+			if (   $user->{is_dbris}
+				or $user->{is_efa}
+				or $user->{is_hafas}
+				or $user->{is_motis} )
+			{
 				return $self->_checkout_journey_p(%opt);
 			}
 

lib/Travelynx/Command/database.pm
@@ -10,6 +10,7 @@ use DateTime;
 use File::Slurp qw(read_file);
 use List::Util  qw();
 use JSON;
+use Travel::Status::DE::EFA;
 use Travel::Status::DE::HAFAS;
 use Travel::Status::DE::IRIS::Stations;
 use Travel::Status::MOTIS;
@@ -3023,6 +3024,19 @@ qq{select distinct checkout_station_id from in_transit where backend_id = 0;}
 			}
 		);
 	},
+
+	# v62 -> v63
+	# Add EFA backend support
+	sub {
+		my ($db) = @_;
+		$db->query(
+			qq{
+				alter table schema_version add column efa varchar(12);
+				update schema_version set version = 63;
+				update schema_version set efa = '0';
+			}
+		);
+	},
 );
 
 sub sync_stations {
@@ -3213,6 +3227,37 @@ sub sync_stations {
 	}
 }
 
+sub sync_backends_efa {
+	my ($db) = @_;
+	for my $service ( Travel::Status::DE::EFA::get_services() ) {
+		my $present = $db->select(
+			'backends',
+			'count(*) as count',
+			{
+				efa  => 1,
+				name => $service->{shortname}
+			}
+		)->hash->{count};
+		if ( not $present ) {
+			$db->insert(
+				'backends',
+				{
+					dbris => 0,
+					efa   => 1,
+					hafas => 0,
+					iris  => 0,
+					motis => 0,
+					name  => $service->{shortname},
+				},
+				{ on_conflict => undef }
+			);
+		}
+	}
+
+	$db->update( 'schema_version',
+		{ efa => $Travel::Status::DE::EFA::VERSION } );
+}
+
 sub sync_backends_hafas {
 	my ($db) = @_;
 	for my $service ( Travel::Status::DE::HAFAS::get_services() ) {
@@ -3228,10 +3273,11 @@ sub sync_backends_hafas {
 			$db->insert(
 				'backends',
 				{
-					iris  => 0,
-					hafas => 1,
-					efa   => 0,
 					dbris => 0,
+					efa   => 0,
+					hafas => 1,
+					iris  => 0,
+					motis => 0,
 					name  => $service->{shortname},
 				},
 				{ on_conflict => undef }
@@ -3258,10 +3304,10 @@ sub sync_backends_motis {
 			$db->insert(
 				'backends',
 				{
-					iris  => 0,
-					hafas => 0,
-					efa   => 0,
 					dbris => 0,
+					efa   => 0,
+					hafas => 0,
+					iris  => 0,
 					motis => 1,
 					name  => $service->{shortname},
 				},
@@ -3359,6 +3405,17 @@ sub migrate_db {
 			}
 			exit(1);
 		}
+	}
+
+	my $efa_version = get_schema_version( $db, 'efa' );
+	say "Found backend table for EFA v${efa_version}";
+	if ( $efa_version eq $Travel::Status::DE::EFA::VERSION ) {
+		say 'Backend table is up-to-date';
+	}
+	else {
+		say
+"Synchronizing with Travel::Status::DE::EFA $Travel::Status::DE::EFA::VERSION";
+		sync_backends_efa($db);
 	}
 
 	my $hafas_version = get_schema_version( $db, 'hafas' );

lib/Travelynx/Command/work.pm
@@ -185,6 +185,85 @@ sub run {
 			next;
 		}
 
+		if ( $entry->{is_efa} ) {
+			eval {
+				$self->app->efa->get_journey_p(
+					trip_id => $train_id,
+					service => $entry->{backend_name}
+				)->then(
+					sub {
+						my ($journey) = @_;
+
+						my $found_dep;
+						my $found_arr;
+						for my $stop ( $journey->route ) {
+							if ( $stop->id_num == $dep ) {
+								$found_dep = $stop;
+							}
+							if ( $arr and $stop->id_num == $arr ) {
+								$found_arr = $stop;
+								last;
+							}
+						}
+						if ( not $found_dep ) {
+							$self->app->log->debug(
+								"Did not find $dep within journey $train_id");
+							return;
+						}
+
+						if ( $found_dep->rt_dep ) {
+							$self->app->in_transit->update_departure_efa(
+								uid     => $uid,
+								journey => $journey,
+								stop    => $found_dep,
+								dep_eva => $dep,
+								arr_eva => $arr,
+								trip_id => $train_id,
+							);
+						}
+
+						if ( $found_arr and $found_arr->rt_arr ) {
+							$self->app->in_transit->update_arrival_efa(
+								uid     => $uid,
+								journey => $journey,
+								stop    => $found_arr,
+								dep_eva => $dep,
+								arr_eva => $arr,
+								trip_id => $train_id,
+							);
+						}
+					}
+				)->catch(
+					sub {
+						my ($err) = @_;
+						$backend_issues += 1;
+						$self->app->log->error(
+"work($uid) @ EFA $entry->{backend_name}: journey: $err"
+						);
+					}
+				)->wait;
+
+				if (    $arr
+					and $entry->{real_arr_ts}
+					and $now->epoch - $entry->{real_arr_ts} > 600 )
+				{
+					$self->app->checkout_p(
+						station => $arr,
+						force   => 2,
+						dep_eva => $dep,
+						arr_eva => $arr,
+						uid     => $uid
+					)->wait;
+				}
+			};
+			if ($@) {
+				$errors += 1;
+				$self->app->log->error(
+					"work($uid) @ EFA $entry->{backend_name}: $@");
+			}
+			next;
+		}
+
 		if ( $entry->{is_motis} ) {
 
 			eval {

lib/Travelynx/Controller/Account.pm
@@ -1077,6 +1077,49 @@ sub backend_form {
 			$backend->{homepage}    = 'https://www.bahn.de';
 			$backend->{recommended} = 1;
 		}
+		elsif ( $backend->{efa} ) {
+			if ( my $s = $self->efa->get_service( $backend->{name} ) ) {
+				$type                = 'EFA';
+				$backend->{longname} = $s->{name};
+				$backend->{homepage} = $s->{homepage};
+				$backend->{regions}  = [ map { $place_map{$_} // $_ }
+					  @{ $s->{coverage}{regions} // [] } ];
+				$backend->{has_area}     = $s->{coverage}{area} ? 1 : 0;
+				$backend->{experimental} = 1;
+
+				if (
+					    $s->{coverage}{area}
+					and $s->{coverage}{area}{type} eq 'Polygon'
+					and $self->lonlat_in_polygon(
+						$s->{coverage}{area}{coordinates},
+						[ $user_lon, $user_lat ]
+					)
+				  )
+				{
+					push( @suggested_backends, $backend );
+				}
+				elsif ( $s->{coverage}{area}
+					and $s->{coverage}{area}{type} eq 'MultiPolygon' )
+				{
+					for my $s_poly (
+						@{ $s->{coverage}{area}{coordinates} // [] } )
+					{
+						if (
+							$self->lonlat_in_polygon(
+								$s_poly, [ $user_lon, $user_lat ]
+							)
+						  )
+						{
+							push( @suggested_backends, $backend );
+							last;
+						}
+					}
+				}
+			}
+			else {
+				$type = undef;
+			}
+		}
 		elsif ( $backend->{hafas} ) {
 
 			# These backends lack a journey endpoint or are no longer

lib/Travelynx/Helper/DBRIS.pm
@@ -94,7 +94,6 @@ sub get_journey_p {
 	my ( $self, %opt ) = @_;
 
 	my $promise = Mojo::Promise->new;
-	my $now     = DateTime->now( time_zone => 'Europe/Berlin' );
 
 	my $agent = $self->{user_agent};
 	my $proxy;

lib/Travelynx/Helper/EFA.pm
@@ -1,0 +1,102 @@
+package Travelynx::Helper::EFA;
+
+# Copyright (C) 2024 Birte Kristina Friesel
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+use strict;
+use warnings;
+use 5.020;
+
+use Travel::Status::DE::EFA;
+
+sub new {
+	my ( $class, %opt ) = @_;
+
+	my $version = $opt{version};
+
+	$opt{header}
+	  = { 'User-Agent' =>
+"travelynx/${version} on $opt{root_url} +https://finalrewind.org/projects/travelynx"
+	  };
+
+	return bless( \%opt, $class );
+}
+
+sub get_service {
+	my ( $self, $service ) = @_;
+
+	return Travel::Status::DE::EFA::get_service($service);
+}
+
+sub get_departures_p {
+	my ( $self, %opt ) = @_;
+
+	my $when = (
+		  $opt{timestamp}
+		? $opt{timestamp}->clone
+		: DateTime->now( time_zone => 'Europe/Berlin' )
+	)->subtract( minutes => $opt{lookbehind} );
+	return Travel::Status::DE::EFA->new_p(
+		service     => $opt{service},
+		name        => $opt{name},
+		datetime    => $when,
+		full_routes => 1,
+		cache       => $self->{realtime_cache},
+		promise     => 'Mojo::Promise',
+		user_agent  => $self->{user_agent}->request_timeout(5),
+	);
+}
+
+sub get_journey_p {
+	my ( $self, %opt ) = @_;
+
+	my $promise = Mojo::Promise->new;
+	my $agent   = $self->{user_agent};
+	my $stopseq;
+
+	if ( $opt{trip_id} =~ m{ ^ ([^@]*) @ ([^@]*) [(] ([^)]*) [)] (.*)  $ }x ) {
+		$stopseq = {
+			stateless => $1,
+			stop_id   => $2,
+			date      => $3,
+			key       => $4
+		};
+	}
+	else {
+		return $promise->reject("Invalid trip_id: $opt{trip_id}");
+	}
+
+	Travel::Status::DE::EFA->new_p(
+		service    => $opt{service},
+		stopseq    => $stopseq,
+		cache      => $self->{realtime_cache},
+		promise    => 'Mojo::Promise',
+		user_agent => $agent->request_timeout(10),
+	)->then(
+		sub {
+			my ($efa) = @_;
+			my $journey = $efa->result;
+
+			if ($journey) {
+				$self->{log}->debug("get_journey_p($opt{trip_id}): success");
+				$promise->resolve($journey);
+				return;
+			}
+			$self->{log}->debug("get_journey_p($opt{trip_id}): no journey");
+			$promise->reject('no journey');
+			return;
+		}
+	)->catch(
+		sub {
+			my ($err) = @_;
+			$self->{log}->debug("get_journey_p($opt{trip_id}): error $err");
+			$promise->reject($err);
+			return;
+		}
+	)->wait;
+
+	return $promise;
+}
+
+1;

commit 79c62872719b4317936ea0bb30a97528ab6a1ea4

geolocation: IRIS stops must be marked es hafas=0
Fixes regression introduced in c250a2f2c7968966014315f76b25109b83c041ed

Closes #242

lib/Travelynx/Controller/Traveling.pm
@@ -773,6 +773,7 @@ sub geolocation {
 			lon      => $_->[0][3],
 			lat      => $_->[0][4],
 			distance => $_->[1],
+			hafas    => 0,
 		}
 	} Travel::Status::DE::IRIS::Stations::get_station_by_location( $lon,
 		$lat, 10 );