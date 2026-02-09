5a951e550fdf8ba80ab8ade8fdd05d7fff31fa87
Birte Kristina Friesel <derf@finalrewind.org>
2026-02-09 21:02:29 +0100

    and another edge case
    Closes #350

--- lib/Travelynx/Helper/HAFAS.pm
+++ lib/Travelynx/Helper/HAFAS.pm
@@ -120,7 +120,8 @@ sub get_tripid_p {
 		$train_desc = 'EC ' . $train->train_no;
 	}
 	elsif ( $train->type
-		=~ m{ ^ (?: ABR | ag | ALX | EB | MRB | NBE | STB | TLX | OE | VIA ) $ }x )
+		=~ m{ ^ (?: ABR | ag | ALX | EB | ERB | MRB | NBE | STB | TLX | OE | VIA ) $ }x
+	  )
 	{
 		$train_desc = $train->train_no;
 	}
