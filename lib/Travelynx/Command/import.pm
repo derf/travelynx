e437d9882fc30614b4737effb606c9d34a41295d
Birte Kristina Friesel <derf@finalrewind.org>
2026-07-27 15:46:06 +0200

    index.pl import: documentation / usability fixes

--- lib/Travelynx/Command/import.pm
+++ lib/Travelynx/Command/import.pm
@@ -27,6 +27,11 @@ sub epoch_to_dt {
 sub import_stops {
 	my ( $self, $csv_filename ) = @_;
 
+	if ( not defined $csv_filename ) {
+		$self->help;
+		return;
+	}
+
 	my $db = $self->app->pg->db;
 
 	open( my $fh, '<:encoding(utf-8)', $csv_filename )
@@ -143,6 +148,11 @@ sub import_stops {
 
 sub import_journeys {
 	my ( $self, $uid, $name, $stops_filename, $json_filename ) = @_;
+
+	if ( not defined $json_filename ) {
+		$self->help;
+		return;
+	}
 
 	my $user_data = $self->app->users->get( uid => $uid );
 
@@ -334,4 +344,4 @@ __END__
 
   Usage: index.pl import stops <stops.csv>
 
-  Usage: index.pl import userdata <uid> <name> <stops.csv> <export.json>
+  Usage: index.pl import journeys <uid> <name> <stops.csv> <export.json>
